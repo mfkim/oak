8c8d929d5cdadbe25316c50b0f4cf37e53a46421
Mingu Kim <mingufkim@gmail.com>
2026-01-03 11:00:00 +0900

    Init: 프로젝트 초기 설정 및 패키지 구조 생성

---
+++ src/main/java/com/oak/server/OakApplication.java
@@ -0,0 +1,13 @@
+package com.oak.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oak/server/OakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oak.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
